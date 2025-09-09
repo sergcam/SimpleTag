eaec4fdc65490797554f1f13a5340e349b7d148f
sergcam <sergiocam43@gmail.com>
2025-09-09 00:36:01 -0700

    add custom font

--- app/src/main/java/dev/secam/simpletag/ui/theme/Theme.kt
+++ app/src/main/java/dev/secam/simpletag/ui/theme/Theme.kt
@@ -1,37 +1,72 @@
+/*
+ * Copyright (C) 2025  Sergio Camacho
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package dev.secam.simpletag.ui.theme
 
+import android.content.Context
+import android.graphics.Color
 import android.os.Build
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.LocalActivity
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
-import dev.secam.simpletag.data.SimpleAppColorScheme
-import dev.secam.simpletag.data.SimpleAppTheme
+import dev.secam.simpletag.data.AppColorScheme
+import dev.secam.simpletag.data.AppTheme
 
 
 @Composable
 fun SimpleTagTheme(
-    appTheme: SimpleAppTheme? = SimpleAppTheme.System,
-    appColorScheme: SimpleAppColorScheme? = SimpleAppColorScheme.Dynamic,
+    appTheme: AppTheme? = AppTheme.System,
+    appColorScheme: AppColorScheme? = AppColorScheme.Dynamic,
     pureBlack: Boolean? = false,
     content: @Composable () -> Unit
 ) {
-    val appColorScheme = appColorScheme ?: SimpleAppColorScheme.Dynamic
+    val appColorScheme = appColorScheme ?: AppColorScheme.Dynamic
     val pureBlack = pureBlack ?: false
     val darkTheme = when(appTheme) {
-        SimpleAppTheme.Dark -> true
-        SimpleAppTheme.Light -> false
+        AppTheme.Dark -> true
+        AppTheme.Light -> false
         else -> isSystemInDarkTheme()
     }
-    val dynamicColor = when(appColorScheme) {
-        SimpleAppColorScheme.Dynamic -> true
-        else -> false
-    }
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
+    val colorScheme = getThemeColorScheme(
+        appColorScheme = appColorScheme,
+        darkTheme = darkTheme,
+        pureBlack = pureBlack,
+        context = LocalContext.current
+    )
+    SetEdge(appTheme)
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
+fun getThemeColorScheme(appColorScheme: AppColorScheme, darkTheme: Boolean, pureBlack: Boolean, context: Context): ColorScheme{
+    return when {
+        appColorScheme == AppColorScheme.Dynamic && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+
             if (darkTheme) {
                 if (pureBlack) blackThemeConvert(dynamicDarkColorScheme(context)) else dynamicDarkColorScheme(context)
             } else dynamicLightColorScheme(context)
@@ -41,10 +76,25 @@ fun SimpleTagTheme(
         darkTheme -> getColorScheme(appColorScheme,true)
         else -> getColorScheme(appColorScheme,false)
     }
+}
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+@Composable
+fun SetEdge(appTheme: AppTheme?) {
+    val activity = LocalActivity.current as ComponentActivity?
+    if(activity != null){
+        when (appTheme) {
+            AppTheme.Dark -> activity.enableEdgeToEdge(
+                SystemBarStyle.dark(Color.TRANSPARENT)
+            )
+
+            AppTheme.Light -> activity.enableEdgeToEdge(
+                SystemBarStyle.light(
+                    Color.TRANSPARENT,
+                    Color.TRANSPARENT
+                )
+            )
+
+            else -> activity.enableEdgeToEdge()
+        }
+    }
 }
--- app/src/main/java/dev/secam/simpletag/ui/theme/Type.kt
+++ app/src/main/java/dev/secam/simpletag/ui/theme/Type.kt
@@ -1,34 +1,76 @@
+/*
+ * Copyright (C) 2025  Sergio Camacho
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package dev.secam.simpletag.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontVariation
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import dev.secam.simpletag.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+@OptIn(ExperimentalTextApi::class)
+val figtreeFamily = FontFamily(
+    Font(
+        resId = R.font.figtree,
+        weight = FontWeight.Normal,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(500)
+        )
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    Font(
+        resId = R.font.figtree,
+        weight = FontWeight.Medium,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(600)
+        )
+    ),
+    Font(
+        resId = R.font.figtree,
+        weight = FontWeight.SemiBold,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(700)
+        )
+    ),
+    Font(
+        resId = R.font.figtree,
+        weight = FontWeight.Bold,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(800)
+        )
     )
-    */
+)
+val baseline = Typography()
+val Typography = Typography(
+        displayLarge = baseline.displayLarge.copy(fontFamily = figtreeFamily),
+        displayMedium = baseline.displayMedium.copy(fontFamily = figtreeFamily),
+        displaySmall = baseline.displaySmall.copy(fontFamily = figtreeFamily),
+        headlineLarge = baseline.headlineLarge.copy(fontFamily = figtreeFamily),
+        headlineMedium = baseline.headlineMedium.copy(fontFamily = figtreeFamily),
+        headlineSmall = baseline.headlineSmall.copy(fontFamily = figtreeFamily),
+        titleLarge = baseline.titleLarge.copy(fontFamily = figtreeFamily),
+        titleMedium = baseline.titleMedium.copy(fontFamily = figtreeFamily),
+        titleSmall = baseline.titleSmall.copy(fontFamily = figtreeFamily),
+        bodyLarge = baseline.bodyLarge.copy(fontFamily = figtreeFamily),
+        bodyMedium = baseline.bodyMedium.copy(fontFamily = figtreeFamily),
+        bodySmall = baseline.bodySmall.copy(fontFamily = figtreeFamily),
+        labelLarge = baseline.labelLarge.copy(fontFamily = figtreeFamily),
+        labelMedium = baseline.labelMedium.copy(fontFamily = figtreeFamily),
+        labelSmall = baseline.labelSmall.copy(fontFamily = figtreeFamily),
 )
