0f153790f9bdecb84284460f39df7a9966fcb239
sergcam <sergiocam43@gmail.com>
2025-09-16 18:18:44 -0700

    add pull to refresh and scroll to top button

--- app/src/main/java/dev/secam/simpletag/ui/selector/ListScreen.kt
+++ app/src/main/java/dev/secam/simpletag/ui/selector/ListScreen.kt
@@ -17,29 +17,33 @@
 
 package dev.secam.simpletag.ui.selector
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.text.input.clearText
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import dev.secam.simpletag.data.MusicData
-import dev.secam.simpletag.data.SortOrder
-import dev.secam.simpletag.data.SortDirection
 import dev.secam.simpletag.ui.selector.components.ListScreenTopBar
 import dev.secam.simpletag.ui.selector.components.SimpleMusicItem
 import dev.secam.simpletag.ui.selector.dialogs.FilterDialog
@@ -49,32 +53,41 @@ import dev.secam.simpletag.ui.selector.dialogs.SortDialog
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListScreen(
-    musicList: List<MusicData>,
     modifier: Modifier = Modifier,
     onNavigateToEditor: (List<MusicData>) -> Unit,
-    updateHasArt: (Int) -> Unit,
-    updateQuery: (String) -> Unit,
-    setSortOrder: (SortOrder, SortDirection) -> Unit,
-    sortOrder: SortOrder,
-    sortDirection: SortDirection,
-    taggedFilter: Boolean,
-    setTaggedFilter: (Boolean) -> Unit,
-    showSortDialog: Boolean,
-    showFilterDialog: Boolean,
-    setShowSortDialog: (Boolean) -> Unit,
-    setShowFilterDialog: (Boolean) -> Unit,
-    filesLoaded: Boolean
+    viewModel: SelectorViewModel,
+    lazyListState: LazyListState
 ) {
+
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
     val searchFieldState = rememberTextFieldState()
     var searchEnabled by remember { mutableStateOf(false) }
     if (!searchEnabled) {
         searchFieldState.clearText()
     }
+    val musicMapState = viewModel.musicMapState.collectAsState().value
 
+    // ui state
+    val uiState = viewModel.uiState.collectAsState().value
+    val musicList = uiState.musicList
+    val sortOrder = uiState.sortOrder
+    val sortDirection = uiState.sortDirection
+    val taggedFilter = uiState.taggedFilter
+    val showSortDialog = uiState.showSortDialog
+    val showFilterDialog = uiState.showFilterDialog
+    val filesLoaded = uiState.filesLoaded
+    val isRefreshing = uiState.isRefreshing
+
+    if (!filesLoaded) {
+        viewModel.loadList()
+    }
+    //  Update list when media repo map changes (e.g. media store refresh etc)
+    LaunchedEffect(musicMapState) {
+        viewModel.syncMusicList()
+    }
     // update sort, filter, and search
     LaunchedEffect(searchFieldState.text, sortOrder, taggedFilter, sortDirection) {
-        updateQuery(searchFieldState.text as String)
+        viewModel.updateMusicList(searchFieldState.text as String)
     }
     Scaffold(
         topBar = {
@@ -85,14 +98,13 @@ fun ListScreen(
                 setSearchEnabled = { enabled ->
                     searchEnabled = enabled
                 },
-                onFilter = { setShowFilterDialog(true)},
-                onSort = { setShowSortDialog(true) }
+                onFilter = { viewModel.setShowFilterDialog(true) },
+                onSort = { viewModel.setShowSortDialog(true) }
             )
         },
         contentWindowInsets = WindowInsets(0),
         modifier = modifier
     ) { contentPadding ->
-
         Column(
             modifier = Modifier
                 .padding(contentPadding)
@@ -104,27 +116,48 @@ fun ListScreen(
                     modifier = Modifier.fillMaxWidth(),
                 )
             }
-
+            //  if list is empty and not loading
+            else if (musicList.isEmpty()) {
+                Text(
+                    text = "No Results Found",
+                    modifier = Modifier
+                        .padding(start = 16.dp)
+                )
+            }
             // File List
             Column(
                 modifier = Modifier
                     .fillMaxSize()
             ) {
-                LazyColumn(
-                    modifier = Modifier
-                        .fillMaxWidth()
+                PullToRefreshBox(
+                    isRefreshing = isRefreshing,
+                    onRefresh = {
+                        viewModel.setIsRefreshing(true)
+                        viewModel.refreshMediaStore()
+                    },
                 ) {
+                    LazyColumn(
+                        state = lazyListState,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
 
-                    items(
-                        count = musicList.size,
-                        key = { musicList[it].id },
-                        contentType = { MusicData }
-                    ) { index ->
-                        if (musicList[index].hasArtwork == null) {
-                            updateHasArt(index)
-                        }
-                        SimpleMusicItem(musicList[index]) {
-                            onNavigateToEditor(listOf(musicList[index]))
+                        items(
+                            count = musicList.size,
+                            key = { musicList[it].id },
+                            contentType = { MusicData }
+                        ) { index ->
+                            Log.d(
+                                "ListScreen",
+                                musicList[index].id.toString() + " " + (musicList[index].title
+                                    ?: "")
+                            )
+                            if (musicList[index].hasArtwork == null) {
+                                viewModel.updateHasArt(index)
+                            }
+                            SimpleMusicItem(musicList[index]) {
+                                onNavigateToEditor(listOf(musicList[index]))
+                            }
                         }
                     }
                 }
@@ -132,18 +165,18 @@ fun ListScreen(
         }
 
         //  Show Dialogs
-        if(showFilterDialog){
+        if (showFilterDialog) {
             FilterDialog(
                 hasTag = taggedFilter,
-                onConfirm = setTaggedFilter
-            ) { setShowFilterDialog(false) }
+                onConfirm = viewModel::setTaggedFilter
+            ) { viewModel.setShowFilterDialog(false) }
         }
-        if(showSortDialog){
+        if (showSortDialog) {
             SortDialog(
                 sortOrder = sortOrder,
                 sortDirection = sortDirection,
-                onConfirm = setSortOrder
-            ) { setShowSortDialog(false) }
+                onConfirm = viewModel::setSort
+            ) { viewModel.setShowSortDialog(false) }
         }
     }
 }
