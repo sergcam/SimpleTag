0ce9617ddd1ef9d2e63f82eaa563c55fc3b0a005
sergcam <sergiocam43@gmail.com>
2025-09-17 18:54:09 -0700

    change version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 27
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "0.1.0-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
