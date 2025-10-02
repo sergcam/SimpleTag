e38fdeb2bdb80da7b2e81f6babca9d0d041a6afa
sergcam <sergiocam43@gmail.com>
2025-10-02 16:26:50 -0700

    update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "dev.secam.simpletag"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.1.5-beta"
+        versionCode = 7
+        versionName = "0.2.0-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
