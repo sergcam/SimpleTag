02455fb26cb18a622a77c7f6956cd061f8349ac0
Sergio Camacho <sergiocam43@gmail.com>
2026-04-28 12:41:26 -0700

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "dev.secam.simpletag"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.3.1-beta"
+        versionCode = 11
+        versionName = "0.3.2-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
